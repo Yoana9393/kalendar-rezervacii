37c6408843755e0dbfb19076137c7e3d8ef98b92
Yoana9393 <yoanadupcheva@gmail.com>
2026-08-11 22:16:51 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kalendar-rezervacii-cloud-hidden-link-v1";
+const CACHE = "kalendar-rezervacii-oblak-mini-v6";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
