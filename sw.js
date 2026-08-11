aac105c65fe2865fe58f92ba8dddc36c29ec2baf
Yoana9393 <yoanadupcheva@gmail.com>
2026-08-11 21:08:05 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = "kalendar-rezervacii-nov-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
